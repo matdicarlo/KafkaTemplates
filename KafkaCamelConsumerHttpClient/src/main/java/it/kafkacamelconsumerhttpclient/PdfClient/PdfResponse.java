35c45f650b7cd163fce9d307c898bcdedcc7ed08
Matteo <mat.dicarlo@gmail.com>
2021-04-14 11:26:34 +0200

    add templates

---
+++ KafkaCamelConsumerHttpClient/src/main/java/it/kafkacamelconsumerhttpclient/PdfClient/PdfResponse.java
@@ -0,0 +1,9 @@
+package it.kafkacamelconsumerhttpclient.PdfClient;
+
+import lombok.Data;import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class PdfResponse {
+    private String pathDelPdf;
+}
